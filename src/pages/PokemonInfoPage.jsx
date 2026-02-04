b228057ac1d719d6e46c885342f4ca270ca00cbd
loganyeh <yehlogan@gmail.com>
2026-02-03 22:46:03 -0500

    feat: static ui blueprint elements lined

--- src/pages/PokemonInfoPage.jsx
+++ src/pages/PokemonInfoPage.jsx
@@ -22,10 +22,9 @@ function PokemonInfoPage(){
                 </div>
 
                 {/* POKEMON NAME AND ID NUMBER */}
-                <div className="border-2 border-red-600 h-2/12 w-full pl-8 flex-col">
-                    <span className="border border-black h-1/2 w-auto text-3xl">#699</span><br />
-                    <span className="border border-black h-1/2 w-auto text-5xl">Aurorus</span>
-                    {/* left of here ************* */}
+                <div className="border-2 border-blue-600 h-2/12 w-full pl-8 flex flex-col ">
+                    <span className="border border-black h-full w-full text-3xl flex justify-start items-end">#699</span>
+                    <span className="border border-black h-full w-full pt-2 text-5xl">Aurorus</span>
                 </div>
 
                 {/* POKEMON PICTURE AND INFO */}
@@ -36,15 +35,17 @@ function PokemonInfoPage(){
 
                         {/* Region TITLE */}
                         <div className="border-2 border-red-600 h-full w-1/12">
-                            <div className="h-1/2 w-full flex justify-center items-center text-3xl whitespace-nowrap rotate-270">Region: Kanto</div>
+                            <div className="h-1/2 w-full flex justify-center items-center text-2xl whitespace-nowrap rotate-270">
+                                <span className="mr-2 text-2xl font-normal">Region: </span><span className="text-xl font-light" >Kanto</span>
+                            </div>
                         </div>
                         
                         {/* NAME TITLE */}
                         <div className="border-2 border-red-600 h-full w-11/12">
-                            <div className="border border-black h-1/3 w-full text-9xl">アマルルガ</div>
-                            <div className="border border-black h-1/3 w-full">
-                                Height: 8.03m<br />
-                                Weight: 90.22kg
+                            <div className="border border-black h-1/3 w-full text-9xl opacity-70">アマルルガ</div>
+                            <div className="border border-black h-1/3 w-full pl-3 text-2xl font-normal">
+                                Height: <span className="text-xl font-light">8.03m</span><br />
+                                Weight: <span className="text-xl font-light">90.22kg</span>
                             </div>
                         </div>
 
@@ -54,20 +55,20 @@ function PokemonInfoPage(){
                     <div className="border-2 border-green-600 h-full w-1/3">
 
                         {/* Pokemon Type */}
-                        <div className="border-2 border-red-600 h-2/12 w-full flex">
-                            <div className="border border-black h-20 w-20 rounded-full">ROCK IMAGE</div>
-                            <div className="border border-black h-20 w-20 rounded-full">SNOW IMAGE</div>
+                        <div className="border-2 border-red-600 h-2/12 w-6/12 flex justify-around items-center">
+                            <div className="border border-black h-28 w-28 flex justify-center items-center rounded-full">ROCK IMAGE</div>
+                            <div className="border border-black h-28 w-28 flex justify-center items-center rounded-full">SNOW IMAGE</div>
                         </div>
                         {/* Base Stats TITLE */}
-                        <div className="border-2 border-red-600 h-2/12 w-full">Base stats:</div>
+                        <div className="border-2 border-red-600 h-2/12 w-full flex justify-start items-center text-6xl">Base stats:</div>
                         {/* Pokemon INFO/STATS */}
-                        <div className="border-2 border-red-600 h-8/12 w-full">
-                            <div className="border border-black h-15 w-60 rounded-xl">HP: 123</div>
-                            <div className="border border-black h-15 w-60 rounded-xl">DEFENSE: 72</div>
-                            <div className="border border-black h-15 w-60 rounded-xl">SP. ATTACK: 99</div>
-                            <div className="border border-black h-15 w-60 rounded-xl">SP. DEFENSE: 92</div>
-                            <div className="border border-black h-15 w-60 rounded-xl">SPEED: 58</div>
-                            <div className="border border-black h-15 w-60 rounded-xl">ATTACK: 77</div>
+                        <div className="border-2 border-red-600 h-auto max-h-80 w-7/12 mt-6 pl-8 flex flex-wrap">
+                            <span className="border border-black h-10 w-auto m-2 px-6 py-4 flex justify-start items-center text-lg font-semibold shadow-md rounded-xl">HP: 123</span>
+                            <span className="border border-black h-10 w-auto m-2 px-6 py-4 flex justify-start items-center text-lg font-semibold shadow-md rounded-xl">DEFENSE: 72</span>
+                            <span className="border border-black h-10 w-auto m-2 px-6 py-4 flex justify-start items-center text-lg font-semibold shadow-md rounded-xl">SP. ATTACK: 99</span>
+                            <span className="border border-black h-10 w-auto m-2 px-6 py-4 flex justify-start items-center text-lg font-semibold shadow-md rounded-xl">SP. DEFENSE: 92</span>
+                            <span className="border border-black h-10 w-auto m-2 px-6 py-4 flex justify-start items-center text-lg font-semibold shadow-md rounded-xl">SPEED: 58</span>
+                            <span className="border border-black h-10 w-auto m-2 px-6 py-4 flex justify-start items-center text-lg font-semibold shadow-md rounded-xl">ATTACK: 77</span>
                         </div>
                     </div>
 
@@ -78,15 +79,15 @@ function PokemonInfoPage(){
                 <div className="border-2 border-red-600 h-1/12 w-full flex justify-around items-start">
 
                     {/* search name */}
-                    <div className="border-2 border-black h-3/4 w-1/3 flex justify-center items-center">
-                        <input type="text" className="border-2 border-black h-full w-3/4" placeholder="Search name"/>
-                        <div className="border-2 border-black h-full w-1/4">Go!</div>
+                    <div className="border-2 border-black h-3/4 w-1/4 flex justify-between items-center">
+                        <input type="text" className="border-2 border-black h-full w-9/12 pl-4 flex justify-center items-center text-xl rounded-xl" placeholder="Search name"/>
+                        <div className="border-2 border-black h-full w-2/12 flex justify-center items-center text-xl rounded-xl">Go!</div>
                     </div>
 
                     {/* search number */}
-                    <div className="border-2 border-black h-3/4 w-1/3 flex justify-center items-center">
-                        <input type="text" className="border-2 border-black h-full w-3/4" placeholder="Search number"/>
-                        <div className="border-2 border-black h-full w-1/4">Go!</div>
+                    <div className="border-2 border-black h-3/4 w-1/4 flex justify-between items-center">
+                        <input type="text" className="border-2 border-black h-full w-9/12 pl-4 flex justify-center items-center text-xl rounded-xl" placeholder="Search number"/>
+                        <div className="border-2 border-black h-full w-2/12 flex justify-center items-center text-xl rounded-xl">Go!</div>
                     </div>
                     
                 </div>
