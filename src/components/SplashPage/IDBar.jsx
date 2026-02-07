d05f405c04061360de21efd77657ec02e6043906
loganyeh <yehlogan@gmail.com>
2026-02-07 08:57:17 -0500

    fix: redesigned nav bar to divs rather than spans

--- src/components/SplashPage/IDBar.jsx
+++ src/components/SplashPage/IDBar.jsx
@@ -23,13 +23,13 @@ function IDBar({ data }){
 
     return(
         <>
-            <div className="border border-black h-1/12 w-full flex justify-center items-start pt-2 text-2xl">
-                <div className="border-2 border-red-600 h-10 w-auto flex justify-center items-center">
-                    <div onClick={handleDecrement} className="border-2 border-black h-full w-20 flex justify-center items-center text-shadow-lg hover:text-gray-200 active:text-white cursor-pointer">&lt;</div>
+            <div className="h-1/12 w-full flex justify-center items-start pt-4 text-2xl">
+                <div className="h-10 w-auto flex justify-center items-center">
+                    <div onClick={handleDecrement} className="h-full w-20 flex justify-center items-center text-shadow-lg hover:text-gray-200 active:text-white cursor-pointer">&lt;</div>
                     {numberLineArr.map((number, index) => {
                         return <IDBarNumber key={index} indexNum={index} num={number} />
                     })}
-                    <div onClick={handleIncrement} className="border-2 border-black h-full w-20 flex justify-center items-center text-shadow-lg hover:text-gray-200 active:text-white cursor-pointer">&gt;</div>
+                    <div onClick={handleIncrement} className="h-full w-20 flex justify-center items-center text-shadow-lg hover:text-gray-200 active:text-white cursor-pointer">&gt;</div>
                 </div>
             </div>
         </>
--- src/components/SplashPage/IDBarNumber.jsx
+++ src/components/SplashPage/IDBarNumber.jsx
@@ -11,7 +11,7 @@ function IDBarNumber({ num, indexNum }){
 
     return(
         <>
-            <div onClick={handleOnClick} className={`border-2 border-black h-full w-20 flex justify-center items-center text-shadow-sm ${(indexNum === 4) ? "font-medium underline" : ""} hover:text-gray-200 active:text-white cursor-pointer`}>{(Number.isNaN(num) || num < 1 || num > 1025) ? "-" : num}</div>
+            <div onClick={handleOnClick} className={`h-full w-20 flex justify-center items-center text-shadow-sm ${(indexNum === 4) ? "font-medium underline" : ""} hover:text-gray-200 active:text-white cursor-pointer`}>{(Number.isNaN(num) || num < 1 || num > 1025) ? "-" : num}</div>
         </>
     )
 }
