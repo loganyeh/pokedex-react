02e4abd8b05a499eac0f88a655f9785494362b59
loganyeh <yehlogan@gmail.com>
2026-02-11 10:00:07 -0500

    redesign pokemon card for home page

--- src/components/Home/PokemonCard.jsx
+++ src/components/Home/PokemonCard.jsx
@@ -1,20 +1,58 @@
+import { useEffect, useState } from "react";
 import { SplashPageColor } from "../../utils/SplashPageColor";
+import { fetchPokemon } from "../../api/api";
 
 function PokemonCard({ data }){
+    const [temp, setTemp] = useState();
+
+    useEffect(() => {
+        const getPokemon = async () => {
+            try {
+                const data = await fetchPokemon(197);
+                setTemp(data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        getPokemon();
+
+    }, []);
 
     return(
         <>
-            <div className={`border-2 border-black ${SplashPageColor(data?.type_one)} h-70 w-60 my-6 rounded-xl shadow-md`}>
-                <div className={`border border-black h-4/5 w-full flex justify-center items-center ${SplashPageColor(data?.type_one)} rounded-t-xl`}>
-                    <img src={data?.sprite} alt={data?.name} className="h-55 w-55" />
+            {/* <div className={`border-2 border-black ${"bg-white"} h-70 w-60 my-6 rounded-xl shadow-md`}>
+                <div className={`border border-black h-4/5 w-full flex justify-center items-center ${"bg-white"} rounded-t-xl`}>
+                    <img src={temp?.sprite} alt={temp?.name} className="h-55 w-55" />
                 </div>
                 <div className="border border-black h-1/5 w-full flex">
-                    <div className="border-2 border-red-600 h-full w-1/5 flex justify-center items-center">#{data?.id}</div>
-                    <div className="border-2 border-red-600 h-full w-3/5 flex justify-center items-center">{data?.name}</div>
+                    <div className="border-2 border-red-600 h-full w-1/5 flex justify-center items-center">#{temp?.id}</div>
+                    <div className="border-2 border-red-600 h-full w-3/5 flex justify-center items-center">{temp?.name}</div>
                     <div className="border-2 border-red-600 h-full w-1/5 flex justify-center items-center">
                         <i className='bx bxs-bookmark-star' ></i>
                     </div>
                 </div>
+            </div> */}
+
+            {/* <img src={temp?.sprite} alt={temp?.name} className="h-55 w-55" /> */}
+
+            <div className={`border border-black ${"bg-white"} h-auto w-100 my-6 rounded-xl shadow-md`}>
+                {/*  */}
+                <div className="border-2 border-red-600 h-12 w-full">#197</div>
+                <div className="border-2 border-red-600 h-12 w-full">Umbreon</div>
+                {/*  */}
+                <div className="border-2 border-red-600 h-24 w-full">
+                    <div className="h-1/3 w-1/3">Region: Kanto</div>
+                    <div className="h-1/3 w-1/3">Height: 3.54m</div>
+                    <div className="h-1/3 w-1/3">Weight: 38.22kg</div>
+                </div>
+                {/*  */}
+                <div className="border-2 border-red-600 h-48 w-full">
+                    <img src={temp?.sprite} alt={temp?.name} className="h-55 w-55" />
+                </div>
+                {/*  */}
+                <div className="border border-red-600 h-24 w-full">ブラッキー</div>
             </div>
         
         </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,11 +27,10 @@ function HomePage(){
             
             catch (error) {
                 console.error(error);
-                
             } 
         };
 
-        getPokemon();
+        // getPokemon();
         
     }, []);
 
@@ -63,11 +62,12 @@ function HomePage(){
                         <div className="border-2 border-red-600 min-h-full w-1/8"></div>
 
                         {/* pokemon list */}
-                        <div className="border-2 border-black min-h-full w-6/8 pb-40 flex flex-wrap justify-between">
+                        <div className="border-2 border-black min-h-full w-6/8 pb-40 flex flex-wrap justify-center items-center">
                             
-                            {cards.map((card, index) => {
+                            {/* {cards.map((card, index) => {
                                 return <PokemonCard key={index} data={card}/>
-                            })}
+                            })} */}
+                            <PokemonCard />
                             
                         </div>  
 
