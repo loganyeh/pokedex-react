106c53ab03db3b4fa9695ad9feddac39db566829
loganyeh <yehlogan@gmail.com>
2026-02-11 10:35:35 -0500

    end: 260211

--- src/components/Home/PokemonCard.jsx
+++ src/components/Home/PokemonCard.jsx
@@ -25,20 +25,26 @@ function PokemonCard({ data }){
             <div className={`border border-gray-200 ${SplashPageColor(data?.type_one)} text-white h-auto w-65 my-6 rounded-xl shadow-md`}>
                 
                 {/* NAME and NUMBER */}
-                <div className="h-8 w-full p-4 text-2xl font-semibold text-shadow-lg">#197</div>
-                <div className="h-8 w-full p-4 text-2xl font-semibold text-shadow-lg">Umbreon</div>
+                <div className="h-8 w-full p-4 text-2xl font-semibold text-shadow-lg flex justify-between">
+                    <div>#{data?.id}</div>
+                    <div className="h-10 w-10 flex justify-center items-center">
+                        <i className='bx bxs-heart text-3xl hover:text-red-500 cursor-pointer' ></i>
+                        {/* left off here ***** */}
+                    </div>
+                </div>
+                <div className="h-8 w-full p-4 text-2xl font-semibold text-shadow-lg">{data?.name}</div>
 
                 {/* H/W REGION INFO */}
                 <div className="h-23 w-full flex">
                     {/* region */}
-                    <div className="h-full w-1/3 flex justify-center items-center text-sm rotate-270 text-shadow-lg">Region: Kanto</div>
+                    <div className="h-full w-1/3 flex justify-center items-center text-sm rotate-270 text-shadow-lg">Region: {data?.region}</div>
                     {/* height and weight */}
                     <div className="h-full w-2/3">
                         <div className="h-1/2 w-full text-shadow-lg"></div>
-                        <div className="h-1/2 w-full text-sm text-shadow-lg">
-                            <span className="font-semibold">Height:</span> 3.54m
+                        <div className="h-1/2 w-full text-md text-shadow-lg">
+                            <span className="font-semibold">Height:</span> {data?.height}m
                             <br />
-                            <span className="font-semibold">Weight:</span> 38.22kg
+                            <span className="font-semibold">Weight:</span> {data?.weight}kg
                         </div>
                     </div>
 
