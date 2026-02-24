9dcc4eb72c7f2d7739bfbd812369ba0b1eabafaa
loganyeh <yehlogan@gmail.com>
2026-02-24 15:11:46 -0500

    feat: favorites page fix: switching pages unreds heart

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { MyContext } from "../context/MyContext";
+import PokemonCard from "../components/Home/PokemonCard";
 
 function Favorites(){
+    const { favorites, setFavorites } = useContext(MyContext);
 
     return(
         <>
             <div className="h-screen w-screen bg-red-300">
-                <div className="border-b border-black h-1/12 w-full flex justify-between items-start text-2xl text-shadow-lg">
-                    <div className="border-r border-black h-full w-1/5 flex justify-around items-center">
+                <div className="h-1/12 w-full flex justify-between items-start text-2xl text-shadow-lg">
+                    <div className="h-full w-1/5 flex justify-around items-center">
                         {/* <Link to={"/home"}><i className='bx bx-home-alt-2 text-5xl text-shadow-lg rounded-full hover:text-gray-300 active:text-gray-100 cursor-pointer'></i></Link> */}
                         <Link to={"/home"}><i className=  'bx bx-home-alt-2 text-5xl text-white rounded-full hover:text-gray-300 active:text-gray-100 cursor-pointer'></i></Link>
                         <Link to={"/"}><i className=  'bx bx-info-circle text-5xl text-white rounded-full hover:text-gray-300 active:text-gray-100 cursor-pointer'></i></Link>
@@ -17,7 +21,7 @@ function Favorites(){
                         <div className="h-10 w-auto flex justify-center items-center text-5xl text-white text-shadow-lg">Favorites</div>
                     </div>
 
-                    <div className="border-l border-black h-full w-1/5 flex justify-center items-center">
+                    <div className="h-full w-1/5 flex justify-center items-center">
                         {/* <Link to={"/"}><i className=  'bx bx-info-circle text-5xl rounded-full hover:text-gray-300 active:text-gray-100 cursor-pointer'></i></Link> */}
                     </div>
 
@@ -25,11 +29,14 @@ function Favorites(){
 
                 {/* body page */}
                 <div className="h-auto min-h-full w-full flex">
-                    <div className="border-r border-black min-h-full w-1/8"></div>
-
-                    {/* pokemon FAVORITES */}
-
-                    <div className="border-l border-black min-h-full w-1/8"></div>
+                    <div className="min-h-full w-1/8"></div>
+                    {/* pokemon list */}
+                    <div className="min-h-full w-6/8 px-10 pb-40 flex flex-wrap justify-between">
+                        {favorites.map((pokemon, index) => {
+                            return <PokemonCard key={index} data={pokemon} />
+                        })}
+                    </div>  
+                    <div className="min-h-full w-1/8"></div>
                 </div>
 
             </div>
